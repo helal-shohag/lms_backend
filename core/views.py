bc44b03bae2e555e498d7f916023ed73b73db8b4
helal <shohag.2896@gmail.com>
2026-01-01 17:57:54 +0600

    view

--- core/views.py
+++ core/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from .models import Category,Course
 from rest_framework.decorators import api_view,permission_classes
-from rest_framework.permissions import IsAuthenticated
-from .models import Category
-from .serializres import CategorySerializer
+from rest_framework.permissions  import IsAuthenticated
+from rest_framework import status
+from .serializres import CategorySerializer,CourseSerializer
 
 
 # Create your views here.
@@ -21,13 +21,29 @@ def categoryview(request):
             serializers = CategorySerializer(data=request.data)
             if serializers.is_valid():
                 serializers.save()
-                return Response(serializers.data,status=201)
+                return Response(serializers.data,status=status.HTTP_201_CREATED)
+            return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+
 @api_view(['GET','POST'])
 def courseview(request):
     if request.method == 'GET':
-        if request.user.role in ['student','admin']:
-            course = Course.objects.all()
-        elif request.user.role == 'instructor':
-            course = Course.objects.filter(instructor=request.user)
-        else:
-            return Response({'error':'Invalid role'},status=403)        
+       if request.user.role in ['admin','student']:
+          course = Course.objects.all()
+       elif request.user.role == 'instructor':
+           course =Course.objects.filter(instructor = request.user)   
+           serializers =CategorySerializer(course,many= True) 
+       else:    
+           return Response(serializers.data)
+    elif request.method == 'POST':
+        if request.user.role != 'admin':
+            return Response({'error': 'Only admin can create Course'},status=status.HTTP_403_FORBIDDEN)
+        serializers = CourseSerializer(data= request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data,status=status.HTTP_201_CREATED)
+            
+            
+   
+
+            
